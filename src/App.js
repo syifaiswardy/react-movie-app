3f25c815ff91341a796e490339d3adf7bc4e9013
Syifa Iswardy <syifaiswardy@gmail.com>
2023-10-07 14:19:26 +0800

    clean code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
 import './index.css';
 import './App.css';
 import Search from './Components/search';
 import MovieList from './Components/movieList';
-import { useContext } from 'react';
-import { GlobalContext } from './GlobalContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Components/home';
 
 function App() {
-
-  const { movieList } = useContext(GlobalContext)
 
   return (
      <div style={{background : 'rgb(19, 23, 53)', fontFamily: 'Roboto Mono'}}>
